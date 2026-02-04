6238d3f0bf669a2434f283f6a4df2b05508e38c0
Oliver Bryan <ob248@proton.me>
2026-02-04 18:49:57 +0000

    formatting fixes

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,14 +1,16 @@
 module.exports = {
-  apps: [{
-    name: 'fonts',
-    script: 'bun',
-    args: 'run dev',
-    interpreter: 'none',
-    watch: true,
-    ignore_watch: ['node_modules', 'logs'],
-    env: {
-      NODE_ENV: 'development'
-    },
-    log_date_format: 'YYYY-MM-DD HH:mm:ss Z'
-  }]
-}
+	apps: [
+		{
+			name: "fonts",
+			script: "bun",
+			args: "run dev",
+			interpreter: "none",
+			watch: true,
+			ignore_watch: ["node_modules", "logs"],
+			env: {
+				NODE_ENV: "development",
+			},
+			log_date_format: "YYYY-MM-DD HH:mm:ss Z",
+		},
+	],
+};
